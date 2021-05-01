dcc61a795c0a245b0aa30861f1d43dd6f96afb30
M.hajializadeh <M.hajializadeh@DESKTOP-PMU0K2R>
2021-05-01 18:26:35 +0430

    Add login and register route

--- MadPay724-Admin/src/app/app.module.ts
+++ MadPay724-Admin/src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PanelModule } from './panel/panel.module';
+import { RouterModule } from '@angular/router';
+import { adminRoutes } from './routes/routes';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PanelModule,
+    AuthModule,
+    RouterModule.forRoot(adminRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
